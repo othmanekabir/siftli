8c44d361b1272d5417d6a1cc6310a59ca35966fd
othmanekabir <othmanekabir1991@gmail.com>
2019-08-19 21:58:37 +0200

    first commit 2

---
+++ src/main/java/fr/siftli/transitmarchandise/TransitmarchandiseApplication.java
@@ -0,0 +1,13 @@
+package fr.siftli.transitmarchandise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitmarchandiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransitmarchandiseApplication.class, args);
+    }
+
+}
